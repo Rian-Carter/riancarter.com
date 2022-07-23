9edcf13f11d4c4151dbe64b30be91f581f50d867
Rian Carter <R.Carter86@hotmail.com>
2022-07-23 15:13:17 -0700

    WIP

--- script.js
+++ script.js
@@ -30,16 +30,16 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
   // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Student", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Student", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing", {
+    //     strings: ["Developer", "Designer", "Student", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
+    // var typed = new Typed(".typing-2", {
+    //     strings: ["Developer", "Designer", "Student", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 });
